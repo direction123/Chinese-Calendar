15dc377b2ef46e3b7962a447fdbf9d04c5982df1
direction123 <direction.wfx@gmail.com>
2017-09-19 18:51:50 -0700

    feat: change theme color

---
+++ app/src/main/java/direction123/calendar/MainActivity.java
@@ -0,0 +1,144 @@
+package direction123.calendar;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import direction123.calendar.R;
+
+/**
+ * Created by fangxiangwang on 9/7/17.
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+    // DrawLayout, Navigation Drawer
+    DrawerLayout mDrawerLayout;
+    NavigationView mDrawerView;
+    ActionBarDrawerToggle mDrawerToggle;
+
+    Toolbar mToolbar;
+    TextView mTitleTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // DrawLayout, Navigation Drawer
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerView = (NavigationView) findViewById(R.id.drawer_view);
+        setupDrawerContent(mDrawerView);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        setupToolbar();
+
+        // Fragment
+     /*   MonthFragment mFragment = new MonthFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, mFragment)
+                .commit(); */
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    /**
+     * Handling the touch event of app icon
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupToolbar(){
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(null);
+
+        mTitleTextView = (TextView) findViewById(R.id.main_toolbar_title);
+        mTitleTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
+    }
+
+    public void setActionBarTitle(String title){
+        mTitleTextView.setText(title);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.nav_setting_fragment:
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                break;
+            case R.id.nav_about_fragment:
+                i = new Intent(this, AboutActivity.class);
+                startActivity(i);
+                break;
+        }
+        // Close the navigation drawer
+        mDrawerLayout.closeDrawers();
+    }
+
+    public void showDatePickerDialog() {
+     //   DialogFragment newFragment = new DatePickerFragment();
+     //   newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+}
+
